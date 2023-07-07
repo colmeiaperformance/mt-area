7d90088bcb24d35e52d0842f5daf025ce2c3a480
Rebeca Marques <beckout@gmail.com>
2023-07-06 21:29:49 -0300

    Update Footer

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
   <div class="content py-0">
     <div class="row fs-sm">
       <div class="col-sm-6 order-sm-2 mb-1 mb-sm-0 text-center text-sm-end">
-        Criado com <i class="fa fa-heart text-danger"></i> por <a class="fw-semibold" href="https://1.envato.market/ydb"
+        Criado com <i class="fa fa-heart text-danger"></i> por <a class="fw-semibold" href="https://www.colmeiaperformance.com.br"
           target="_blank">
           <img class="img-fluid img-logo-footer" src="<?php echo get_template_directory_uri() . '/assets/media/mt/logo-colmeia-dark.png'; ?>" alt="Colmeia Performance">
         </a>
       </div>
       <div class="col-sm-6 order-sm-1 text-center text-sm-start">
-        <a class="fw-semibold" href="https://1.envato.market/r6y" target="_blank">Meditação Transcendental</a> &copy;
+        <a class="fw-semibold" href="https://www.meditacaotranscendental.com.br" target="_blank">Meditação Transcendental</a> &copy;
         <span data-toggle="year-copy"></span>
       </div>
     </div>
