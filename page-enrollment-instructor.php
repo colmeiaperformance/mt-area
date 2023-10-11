0ecf84ba4380ae0e0b39a9a9e3c0dc57f64917fa
Rebeca Marques <beckout@gmail.com>
2023-10-10 22:46:04 -0300

    [Feat] New pages structure

---
+++ page-enrollment-instructor.php
@@ -0,0 +1,31 @@
+<?php include("header.php"); ?>
+
+<!-- Main Container -->
+<main id="main-container">
+  <?php get_template_part( '/template-parts/quick-menu' ) ?>
+
+  <!-- Hero -->
+  <div class="bg-image bg-image-fixed"
+    style="background-image: url('<?php echo get_template_directory_uri() . '/assets/media/mt/qm_cursos.jpg'; ?>');">
+    <div class="bg-mt-default-op">
+      <div class="content content-full text-center">
+        <h1 class="fw-bold text-white mt-3 mb-2">
+          Painel de Controle - Instrutor
+        </h1>
+        <h3>Lista de todos os cursos/eventos do instrutor</h3>
+      </div>
+    </div>
+  </div>
+  <!-- END Hero -->
+
+  <!-- Page Content -->
+  <div class="content">
+
+    
+
+  </div>
+  <!-- END Page Content -->
+</main>
+<!-- END Main Container -->
+
+<?php include("footer.php"); ?>
